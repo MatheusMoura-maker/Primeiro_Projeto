39bfc2df775e2ef230252d3f71f296d2486dbeb9
Matheus Moura <maatmoura@gmail.com>
2026-03-15 21:53:12 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<?php
+echo  "<h1>BACKEND<br></h1>";
+echo "Hello,World!<br>";
+echo "Banco de dados<br>";
+$Nome="Matheus de Moura";
+$Carro="Hyundai I30";
+$gasolina="Gasolina aditivada do posto shell";
+
+
+echo "Meu nome é $Nome, meu carro é um $Carro e eu abasteço com $gasolina";
+
+
+?>
+
+</html>
