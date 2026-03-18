7e24b675b7d5def5e1170bebc92e3a95defe03b9
Matheus Moura <maatmoura@gmail.com>
2026-03-17 21:22:55 -0300

    PROJETO DE EXTENÇÃO

--- index.php
+++ index.php
@@ -2,18 +2,16 @@
 
 <?php
 echo  "<h1>BACKEND<br></h1>";
-echo "Hello,World!<br>";
+
+echo "Hello,World!<br>";                                                                
 echo "Banco de dados<br>";
 $Nome="Matheus de Moura";
 $Carro="Hyundai I30";
 $gasolina="Gasolina aditivada do posto shell";
-$macaco="chimpanzé albino";
-$Dinossauro="Tiranossauro Rex";
+echo "matheus de moura<br>";
+
 
 echo "Meu nome é $Nome, meu carro é um $Carro e eu abasteço com $gasolina";
-echo "O meu macaco é um $macaco";
-echo "O meu dinossauro é um $Dinossauro";
-echo "minha vida é dificil encontrei um $macaco e um $Dinossauro na minha casa";
 
 
 ?>
